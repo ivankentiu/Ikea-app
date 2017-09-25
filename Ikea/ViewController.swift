d6268374b9dcffa396b0bcb5e5b7d90522b304c2
ivantiu <ivankentiu@gmail.com>
2017-09-25 18:45:06 +0800

    add Items

--- Ikea/ViewController.swift
+++ Ikea/ViewController.swift
@@ -11,18 +11,24 @@ import ARKit
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    // string matches filename in terms of spelling
     let itemsArray: [String] = ["cup", "vase", "boxing", "table"]
     @IBOutlet weak var itemsCollectionView: UICollectionView!
     @IBOutlet weak var sceneView: ARSCNView!
     
     let configuration = ARWorldTrackingConfiguration()
+    var selectedItem: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
         self.sceneView.session.run(configuration)
+        self.configuration.planeDetection = .horizontal
         self.itemsCollectionView.dataSource = self
         self.itemsCollectionView.delegate = self
+        
+        // make sure to register recognizers!
+        self.registerGestureRecognizers()
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,6 +41,38 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         return itemsArray.count
     }
     
+    func registerGestureRecognizers() {
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func tapped(sender: UITapGestureRecognizer) {
+        // on horizontal / ARSCNView bec not object like previous jellyfish(SCNView)
+        let sceneView = sender.view as! ARSCNView
+        let tapLocation = sender.location(in: sceneView)
+        let hitTest = sceneView.hitTest(tapLocation, types: .existingPlaneUsingExtent)
+        
+        // if not empty
+        if !hitTest.isEmpty {
+            self.addItem(hitTestResult: hitTest.first!)
+        }
+    }
+    
+    func addItem(hitTestResult: ARHitTestResult) {
+        // from didSelect ItemAt (if not nil navigate to scene which has that item)
+        if let selectedItem = self.selectedItem {
+            // load scene of item we want to place
+            let scene = SCNScene(named: "Models.scnassets/\(selectedItem).scn")
+            // get the node inside of scn (parenthesis node is optional (unwrap)
+            let node = (scene?.rootNode.childNode(withName: selectedItem, recursively: false))!
+            let transform = hitTestResult.worldTransform
+            let thirdColumn = transform.columns.3
+            node.position = SCNVector3(thirdColumn.x, thirdColumn.y, thirdColumn.z)
+            self.sceneView.scene.rootNode.addChildNode(node)
+        }
+    }
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "item", for: indexPath) as! itemCell
         cell.itemLabel.text = self.itemsArray[indexPath.row]
@@ -44,6 +82,8 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     // from UICollectionViewDelegate (whenever select a certain cell)
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let cell = collectionView.cellForItem(at: indexPath)
+        // selected items base on name of values in array
+        self.selectedItem = itemsArray[indexPath.row]
         cell?.backgroundColor = UIColor.green
     }
     
