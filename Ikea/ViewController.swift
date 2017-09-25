20c3eb3ec32766a0f7a04a00fcbfb75a37d0947f
ivantiu <ivankentiu@gmail.com>
2017-09-25 17:23:49 +0800

    Collection UI Basics

---
+++ Ikea/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  Ikea
+//
+//  Created by Ivan Ken Tiu on 25/09/2017.
+//  Copyright © 2017 FinalShift Inc. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    let itemsArray: [String] = ["cup", "vase", "boxing", "table"]
+    @IBOutlet weak var itemsCollectionView: UICollectionView!
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.session.run(configuration)
+        self.itemsCollectionView.dataSource = self
+        self.itemsCollectionView.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // from UICollectionViewDataSource
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return itemsArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "item", for: indexPath) as! itemCell
+        cell.itemLabel.text = self.itemsArray[indexPath.row]
+        return cell
+    }
+
+    // from UICollectionViewDelegate (whenever select a certain cell)
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.green
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.orange
+    }
+}
+
---
+++ Ikea/itemCell.swift
@@ -0,0 +1,14 @@
+//
+//  itemCell.swift
+//  Ikea
+//
+//  Created by Ivan Ken Tiu on 25/09/2017.
+//  Copyright © 2017 FinalShift Inc. All rights reserved.
+//
+
+import UIKit
+
+class itemCell: UICollectionViewCell {
+    
+    @IBOutlet weak var itemLabel: UILabel!
+}
